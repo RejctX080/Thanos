68433799315ab6f95d09d8d8b67b8f4295547ca7
RejctX080 <97124200+RejctX080@users.noreply.github.com>
2023-01-07 15:28:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 10248430
-    API_HASH = "42396a6ff14a569b9d59931643897d0d"
+    APP_ID = 12822418
+    API_HASH = "46599e1778bcdf6dd6b2d4daaf8c968b"
     # the name to display in your alive message
     ALIVE_NAME = "rishabh"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
@@ -14,7 +14,7 @@ class Development(Config):
     # create a new bot in @botfather and fill the following vales with bottoken
     TG_BOT_TOKEN = "5983368873:AAHIQJHRS8hcvDVGAHeuBIko-opFGgVQQa0"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -1001700467435
+    PRIVATE_GROUP_BOT_API_ID = -1001479829328
     # command handler
     COMMAND_HAND_LER = ","
     VCMODE = "True"
